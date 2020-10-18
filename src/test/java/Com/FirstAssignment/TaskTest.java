2509fce9c74a5de1d415d5e8bdd37afb3a51c009
meghaCV <megha.cv27@gmail.com>
2020-10-18 13:57:18 +0200

    initialise test for the task.java class file

---
+++ src/test/java/Com/FirstAssignment/TaskTest.java
@@ -0,0 +1,52 @@
+package Com.FirstAssignment;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+
+    @Test
+    public void getTaskTitle() {}
+
+    @Test
+    void getprojectName() {
+    }
+
+    @Test
+    void setTaskTitle() {
+    }
+
+    @Test
+    void setProjectName() {
+    }
+
+    @Test
+    void isCompleted() {
+    }
+
+    @Test
+    void markComplete() {
+
+    }
+
+    @Test
+    void getDueDate() {
+    }
+
+    @Test
+    void setDueDate() {
+    }
+
+    @Test
+    void taskDisplayFormatted() {
+    }
+}
